347f2484df558667f04de6fdbaf6428cca3ef3af
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-05-18 14:58:11 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className=""></div>
+  return (
+    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
+      <h1 className="text-3xl font-bold">Sua cápsula do tempo.</h1>
+    </div>
+  )
 }
