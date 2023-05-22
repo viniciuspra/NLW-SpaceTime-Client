68565874a803114b80e0d14997c8e33f71e1aabc
viniciuspra <93842439+viniciuspra@users.noreply.github.com>
2023-05-21 22:19:16 -0300

    Save tokens in cookies

---
+++ src/components/Profile.tsx
@@ -0,0 +1,25 @@
+import { getUser } from '@/lib/auth'
+import Image from 'next/image'
+
+export function Profile() {
+  const { name, avatarUrl } = getUser()
+
+  return (
+    <div className="flex items-center gap-3 text-left">
+      <Image
+        src={avatarUrl}
+        width={48}
+        height={48}
+        alt=""
+        className="h-12 w-12 rounded-full"
+      />
+
+      <p className="max-w-[140px] text-sm font-bold leading-snug">
+        {name}
+        <a href="" className="block text-red-400 hover:text-red-300">
+          Quero sair
+        </a>
+      </p>
+    </div>
+  )
+}
